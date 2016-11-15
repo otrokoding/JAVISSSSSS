79c7b0b969d11bcf40c1be9f333d97d7573bbf89
otrokoding <javi.fra@live.com>
2016-11-15 08:51:53 -0400

    Update pro.py

--- pro.py
+++ pro.py
@@ -1,3 +1,14 @@
+def genera ():
+  l=range [1,42]
+  mi_lista=[]
+  while len (mi_lista)<6:
+    numero=random.choice(l)
+
+if not (numero) in mi_lista:
+  mi_lista.append(numero)
+
+return mi_lista
+
 def comprueba (X,L): 
   if x in L:
     return false 
